186d3304276e015160c4c5187d12a17183222774
olivebishop <olivehendrilgen1@gmail.com>
2025-02-24 05:53:28 +0300

    chore: git configs

--- app/(main)/beds/page.tsx
+++ app/(main)/beds/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 
+
 function CTABanner() {
   return (
     <section className="py-12 bg-gradient-to-r from-black to-[#171717] text-center">
