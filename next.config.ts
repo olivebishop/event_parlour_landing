90a7fa76324b2d6695fa0c04a3ec8244ed3468e1
olivebishop <olivehendrilgen1@gmail.com>
2025-03-03 09:53:16 +0300

    fix: fixes by adding way to remove console in production

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from 'next';
 
 // Specify the custom path to your i18n/request.ts file
 const withNextIntl = createNextIntlPlugin('./app/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig: NextConfig = {
+    compiler:{
+        removeConsole: process.env.NODE_ENV === 'production'
+    }
+};
 
 export default withNextIntl(nextConfig);
