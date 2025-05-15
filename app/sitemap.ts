dbf4734463c9e63701213613f7e0859fd6d308ff
olivebishop <olivehendrilgen1@gmail.com>
2025-05-16 00:20:56 +0300

    fix(sitemap): fixes on sitemap and adding the right path fixes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,6 +9,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+        url: 'https://eventparlour.com/why-us',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
         url: 'https://eventparlour.com/contact-us',
       lastModified: new Date(),
       changeFrequency: 'monthly',
