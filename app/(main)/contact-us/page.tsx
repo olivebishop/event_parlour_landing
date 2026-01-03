7510b3b3523018ad0d3d681d4c218cad18cfc63e
olivebishop <olivehendrilgen1@gmail.com>
2026-01-03 05:29:07 +0300

    commented pages not in use

--- app/(main)/contact-us/page.tsx
+++ app/(main)/contact-us/page.tsx
@@ -1,13 +1,13 @@
-"use client"
+// "use client"
 
-import { ContactUs } from "@/components/shared/contactUs/Contact"
-import { motion } from "framer-motion"
+// import { ContactUs } from "@/components/shared/contactUs/Contact"
+// import { motion } from "framer-motion"
 
-export default function ContactUsPage() {
-  return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
-      <ContactUs />
-    </motion.div>
-  )
-}
+// export default function ContactUsPage() {
+//   return (
+//     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
+//       <ContactUs />
+//     </motion.div>
+//   )
+// }
 
